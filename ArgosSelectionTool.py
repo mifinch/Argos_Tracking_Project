ff1f1d27c790e63bd08eef3848049c88d8f550f2
mifinch <mif15@duke.edu>
2026-09-21 13:08:31 -0400

    initial commit

---
+++ ArgosSelectionTool.py
@@ -0,0 +1,12 @@
+#-------------------------------------------------------------
+# ArgosSelectionTool.py
+#
+# Description: Reads in an Argos tracking data file and allows
+#   the user to identify the tracked sitings found within a 
+#   specified bounding box.
+#
+# Author: Morgan Finch (mif15@duke.edu)
+# Date:   Fall 2026
+#--------------------------------------------------------------
+
+
